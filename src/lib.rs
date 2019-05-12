d620c961bfb668e8a6041c5ad9b591ab3cb81928
Davarice <jsdreim@gmail.com>
2019-05-12 03:47:23 -0400

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use pyo3::types::{PyBytes, PyDict, PyByteArray};
+
+fn main() {
+    extern crate pyo3;
+    use pyo3::prelude::*;
+    use pyo3::wrap_pyfunction;
+
+    /// Split a raw IRCv3 line into a usable Dict.
+    #[pyfunction]
+    fn decode(line: &str) -> PyResult<PyDict> {
+        // First, initialize the Dict.
+        let gil = Python::acquire_gil();
+        let py: Python = gil.python();
+        let dict: &PyDict = PyDict::new(py);
+        // Then, break the line down.
+        // TODO: Break apart the String and fill the Dict.
+        Ok(dict)
+    }
+
+    /// A module for manipulation of IRCv3 data.
+    #[pymodule]
+    fn ircsplit(_py: Python, m: &PyModule) -> PyResult<()> {
+        m.add_wrapped(wrap_pyfunction!(decode))?;
+
+        Ok(())
+    }
+}
