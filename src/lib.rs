a850068eca5ed3be39a3dd16aba5c31abc2c3069
Davarice <jsdreim@gmail.com>
2019-05-12 05:04:21 -0400

    Reorganize and prepare

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,40 @@
-use pyo3::types::{PyBytes, PyDict, PyByteArray};
+extern crate pyo3;
 
-fn main() {
-    extern crate pyo3;
-    use pyo3::prelude::*;
-    use pyo3::wrap_pyfunction;
+use pyo3::prelude::*;
+use pyo3::types::{PyBytes, PyDict};
+use pyo3::wrap_pyfunction;
+use std::str;
 
-    /// Split a raw IRCv3 line into a usable Dict.
-    #[pyfunction]
-    fn decode(line: &str) -> PyResult<PyObject> {
-        // First, initialize the Dict.
-        let gil = Python::acquire_gil();
-        let py: Python = gil.python();
-        let dict: &PyDict = PyDict::new(py);
-        // Then, break the line down.
-        // TODO: Break apart the String and fill the Dict.
-        for i in 0..line.len() {
-            dict.set_item(i, i + 2);
-        }
-        Ok(dict.into())
+/// Split a raw IRCv3 line into a usable Dict.
+#[pyfunction]
+fn decode(input: &PyBytes) -> PyResult<PyObject> {
+    // First, decode the data into something we can work.
+    let line: &str = match str::from_utf8(input.as_bytes()) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    // Then, initialize the Dict.
+    let gil = Python::acquire_gil();
+    let py: Python = gil.python();
+    let dict: &PyDict = PyDict::new(py);
+
+    // Third, break the line down.
+    // TODO: Break apart the String.
+
+    // Finally, populate the Dict with all the values.
+    for i in 0..line.len() {
+        // FIXME: Placeholder routine until String breaking is ready.
+        dict.set_item(i, line)?;
     }
 
-    /// A module for manipulation of IRCv3 data.
-    #[pymodule]
-    fn ircsplit(_py: Python, m: &PyModule) -> PyResult<()> {
-        m.add_wrapped(wrap_pyfunction!(decode))?;
+    Ok(dict.into())
+}
 
-        Ok(())
-    }
+/// A module for manipulation of IRCv3 data.
+#[pymodule]
+fn ircsplit(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(decode))?;
+
+    Ok(())
 }
